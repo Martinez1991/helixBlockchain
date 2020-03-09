14a76488ffae5b49bab460ee3eb32544d969c0ef
Martinez1991 <46902047+Martinez1991@users.noreply.github.com>
2020-03-09 00:06:10 -0300

    Add files via upload

---
+++ Block.py
@@ -0,0 +1,108 @@
+import hashlib
+import time
+import binascii
+
+
+class Block:
+    def __init__(self, index, previousHash, timestamp, data, hash, difficulty, nonce):
+        self.index = index
+        self.previousHash = previousHash
+        self.timestamp = timestamp
+        self.data = data
+        self.hash = hash
+        self.difficulty = difficulty
+        self.nonce = nonce
+
+    def print_bloco(self):
+        print("Índice:", self.index)
+        print("Hash anterior:", self.previousHash)
+        print("Timestamp:", self.timestamp)
+        print("Dados:", self.data)
+        print("Hash:", self.hash)
+        print("Dificuldade:", self.difficulty)
+        print("Nonce:", self.nonce)
+
+class Blockchain:
+    def __init__(self, genesisBlock):
+        self.__chain = []
+        self.__chain.append(genesisBlock)
+        self.DIFFICULTY_ADJUSTMENT = 50
+        self.BLOCK_INTERVAL = 120
+
+    def getLastestBlock(self):
+        return self.__chain[len(self.__chain) - 1]
+
+    def generateNextBlock(self, data):
+        previousBlock = self.getLastestBlock()
+        nextIndex = previousBlock.index + 1
+        nextTimestamp = int(round(time.time() * 1000))
+        nextPreviousHash = previousBlock.hash
+        nextdifficulty = self.getDifficulty()
+        nonce = str(0)
+        newBlock = Block(nextIndex, nextPreviousHash, nextTimestamp, data,
+                         calculateHash(nextIndex, nextPreviousHash, nextTimestamp, data, nextdifficulty, nonce),
+                         nextdifficulty, nonce)
+        if self.validatingBlock(newBlock) == True:
+            self.__chain.append(self.findBlock(nextIndex, nextPreviousHash, nextTimestamp, data, nextdifficulty))
+
+
+    def validatingBlock(self, newBlock):
+        previousBlock = self.getLastestBlock()
+        if previousBlock.index + 1 != newBlock.index:
+            return False
+        elif previousBlock.hash != newBlock.previousHash:
+            return False
+        return True
+
+    def hashMatchesDifficulty(self, hash, difficulty):
+        hashBinary = binascii.unhexlify(hash)
+        requiredPrefix = '0'*int(difficulty)
+        requiredPrefix = binascii.a2b_hex(requiredPrefix.strip())
+        return hashBinary.startswith(requiredPrefix)
+
+
+    def findBlock(self, index, previousHash, timestamp, data, difficulty):
+        nonce = 0
+        while True:
+            hash = calculateHash(index, previousHash, timestamp, data, difficulty, nonce)
+            if self.hashMatchesDifficulty(hash, self.getDifficulty()):
+                block = Block(index, previousHash, timestamp, data, hash, difficulty, nonce)
+                return block
+            nonce = nonce + 1
+
+    def getDifficulty(self):
+        latestBlock = self .getLastestBlock()
+        if latestBlock.index % self.DIFFICULTY_ADJUSTMENT == 0 and latestBlock.index != 0:
+            return self.getAdjustedDifficulty()
+        else:
+            return latestBlock.difficulty
+
+
+    def getAdjustedDifficulty(self):
+        latestBlock = self.getLastestBlock()
+        prevAdjustmentBlock = self.__chain[len(self.__chain) - self.DIFFICULTY_ADJUSTMENT]
+        timeExpected = self.BLOCK_INTERVAL * self.DIFFICULTY_ADJUSTMENT
+        timeTaken = latestBlock.timestamp - prevAdjustmentBlock.timestamp
+        if timeTaken < timeExpected * 2:
+            return prevAdjustmentBlock.difficulty + 1
+        elif timeTaken > timeExpected * 2:
+            return prevAdjustmentBlock.difficulty - 1
+        else:
+            return  prevAdjustmentBlock.difficulty
+
+    def printBlockChain(self):
+        for i in range(0, len(self.__chain)):
+            print(self.__chain[i])
+
+    def print_blocos(self):
+        for bloco in self.__chain:
+            bloco.print_bloco()
+            print()
+
+
+def calculateHash(index, previousHash, timestamp, data, difficulty, nonce):
+    return hashlib.sha256((str(index) + previousHash + str(timestamp) + data + str(difficulty) + str(nonce)).encode('utf-8')).hexdigest()
+
+ts = int(round(time.time() * 1000))
+
+
---
+++ Funcoes.py
@@ -0,0 +1,143 @@
+from pymongo import MongoClient
+import threading
+import csv
+from pprint import pprint
+import time
+
+
+# função conectaEntidade() conecta e trás  todos os dados do Orion e salva uma Lista
+def conectaEntidade(ip, porta):
+    client = MongoClient(ip, porta)
+    mydb = client["orion"]
+    mycol = mydb["entities"]
+    return mycol
+
+
+# função conectaCsubs() conecta e trás  todos os dados do Orion e salva uma Lista
+def conectaCsubs(ip, porta):
+    client = MongoClient(ip, porta)
+    mydb = client["orion"]
+    mycol = mydb["csubs"]
+    return mycol
+
+
+
+# função gravaDB() grava todos os dados do MongoDB em uma Lista
+def gravaDB(entities):
+    cursor = entities.find()
+    lista = []
+    for linha in cursor:
+        lista.append(linha)
+    return lista
+
+
+# função monitora() monitora para ver se houve atualização de qualquer dado do MongoDB.
+def monitora(dados, lastreslt=[]):
+    myresult = dados
+    prova = []
+    for x in myresult:
+        if x not in lastreslt:
+            lastreslt.append(x)
+            prova.append(x)
+    return prova
+
+
+# função trans1() trás o Id e o valor do divice "csubs" do MongoDB, cujo os dados são dos IoTs FEDERADOS.
+def dadosFederados(csubs,entities):
+    cursor2 = csubs.distinct("entities.id")
+    lista = []
+    for document in cursor2:
+        a = entities.find_one({"_id.id": document}, {"attrs.temperature.value"})
+        lista.append(a)
+    return lista
+
+
+# função getDadosProximoHelix() trás todos os dados do "entities" do MongoDB do helix a ser validado
+def getDadosProximoHelix(entidades):
+    lista1 = []
+    cursor1 = entidades.distinct("_id.id")
+    for lin in cursor1:
+        a = entidades.find_one({"_id.id": lin}, {"attrs.temperature.value"})
+        lista1.append(a)
+    return lista1
+
+
+
+# função validaFederados() valida se o dado do 1ª banco de dados está igual ao do 2ª
+def validaFederados(csubs,entidade):
+    helix1 = csubs
+    helix2 = entidade
+    listaTest = []
+    for x in helix1:
+        if x in helix2:
+            listaTest.append(x)
+        else:
+            a = str(x)
+            a = a.split("'")[5]
+            print('~' * 40)
+            print(f'    {"Divice " + a + " Foi adulterado"}')
+            print('~' * 40)
+    return listaTest
+
+
+
+# função atualiza() valida se o dado será o não grava na Blockchain
+def atualizar(dados, chain):
+    lista = dados
+    if lista != []:
+        for i in range(0, len(lista)):
+            test = lista[i]
+            chain.generateNextBlock(str(test))
+
+
+
+
+# função findFederado() procura os Ip dos outros Brokers Federados
+def findFederado(csubs):
+    cursor2 = csubs.distinct("reference")
+    lista = []
+    for document in cursor2:
+        srt = str(document)
+        srt = srt.split("/")[2]
+        srt = srt.split(":")[0]
+        lista.append(srt)
+    return lista
+
+
+def Federa(ip, helix1Csub, helix1Ent, bc):
+
+    if ip != []:
+
+        for linha in ip:
+
+            helix2Ent = conectaEntidade(linha, 27017)
+
+            helix2Csub = conectaCsubs(linha, 27017)
+
+            dadosEntHelix2 = getDadosProximoHelix(helix2Ent)
+
+            dadosFederado = dadosFederados(helix1Csub, helix1Ent)
+
+            validaFederado = validaFederados(dadosFederado, dadosEntHelix2)
+
+            monitoraFederado = monitora(validaFederado)
+
+            atualizar(monitoraFederado, bc)
+
+            proc = procuraId(helix1Ent, helix2Csub)
+
+    return proc
+
+
+# função findFederado() procura os Ip dos outros Brokers Federados e valida se está nno primeiro broker
+def procuraId(ent, csub):
+    cursor2 = ent.distinct("entities.id")
+    lista = []
+    for document in cursor2:
+        a = csub.find_one({"_id.id": document}, {"reference"})
+        for x in a:
+            srt = str(x)
+            srt = srt.split("/")[2]
+            srt = srt.split(":")[0]
+            lista.append(srt)
+    return lista
